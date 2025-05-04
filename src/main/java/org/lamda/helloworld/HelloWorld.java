b22114d27cd74e6cfcd5089e6ebfb3e934313860
Mrigraj2 <20201746@sbsstc.ac.in>
2025-05-04 21:25:50 +0530

    Write logs or anything you desire from lambda to efs

--- src/main/java/org/lamda/helloworld/HelloWorld.java
+++ src/main/java/org/lamda/helloworld/HelloWorld.java
@@ -1,5 +1,8 @@
 package org.lamda.helloworld;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -7,6 +10,16 @@ package org.lamda.helloworld;
 public class HelloWorld
 {
     public String handler(){
+
+        String LOG_FILE = "/mnt/logs/lambda_logs.txt";
+        try (FileWriter writer = new FileWriter(LOG_FILE, true)) {
+            writer.write("Lambda executed");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
         return "Hello world";
     }
 }
